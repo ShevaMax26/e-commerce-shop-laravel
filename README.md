c76871f196c76d09dadc745e6b7fde820216ce4b
ShevaMax26 <shevchuk.maxym@gmail.com>
2023-12-05 16:27:41 +0200

    start

---
+++ README.md
@@ -0,0 +1,17 @@
+## Dev tips
+php artisan migrate
+```
+docker-compose build --no-cache
+docker-compose up --build --force-recreate --no-deps
+
+docker-compose down
+
+docker-compose up -d
+
+docker exec -it e_commerce_shop_laravel_app bash
+
+//не заходячи в bash, просто в папці проекту перший раз дати пермішини
+chmod -R 777  ./
+
+systemctl restart docker
+```
---
+++ app/Models/User.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+
+class User extends Authenticatable implements FilamentUser
+{
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    public function addresses(): HasMany
+    {
+        return $this->hasMany(UserAddress::class);
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->canAccessAdminPanel();
+    }
+
+    public function scopeFilamentUsers(Builder $q): Builder
+    {
+        return $q->whereHas('roles', function (Builder $q) {
+            $q->whereIn('name', ['super admin', 'admin', 'writer', 'manager']);
+        });
+    }
+
+    public function canAccessAdminPanel(): bool
+    {
+        return $this->hasRole(['super admin', 'admin', 'writer', 'manager']);
+    }
+}
